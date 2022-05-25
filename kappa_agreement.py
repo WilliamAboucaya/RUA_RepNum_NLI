3abcd160481bcb7ba53a84619316f5745c3364ac
Oana Balalau <oana.balalau@inria.fr>
2022-05-25 14:06:15 +0000

    Upload New File

---
+++ kappa_agreement.py
@@ -0,0 +1,29 @@
+from sklearn.metrics import cohen_kappa_score
+import csv
+import sys
+
+if __name__ == '__main__':
+    oana_file = sys.argv[1]
+    william_file = sys.argv[2]
+    oana = []
+    william = []
+    annotations = []
+    with open(oana_file, 'r') as data:
+        for line in csv.DictReader(data):
+            #print(line["label"])
+            if len(line["label"]):
+                oana.append(line["label"])
+                annotations.append(line)
+
+    with open(william_file, 'r') as data:
+        for line in csv.DictReader(data, delimiter=';'):
+            #print(line["label"])
+            william.append(line["label"])
+
+    print(cohen_kappa_score(oana, william[0:len(oana)]))
+    print(len(oana))
+    for i in range(0, len(oana)):
+        print(str(oana[i]) + str(william[i]))
+        if oana[i] != william[i]:
+            print(annotations[i])
+            print("\n")
